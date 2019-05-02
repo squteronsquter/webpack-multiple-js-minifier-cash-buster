45d344fc9eaf6ecc25aed79ca1c8b23d25f5fcc1
squteronsquter <squter@squter.pl>
2019-05-02 20:17:18 +0200

    Minification of Css, JS and HTML added

--- webpack.common.js
+++ webpack.common.js
@@ -1,22 +1,13 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: {
     main: './src/index.js',
     vendor: './src/vendor.js',
     library: './src/library.js'
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/template.html'
-    })
-  ],
   module: {
     rules: [
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      },
       {
         test: /\.html$/,
         use: ['html-loader']
